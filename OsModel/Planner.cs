0965fc54727a492b30120cdab58b910f0a0efb8d
Julius.Mazionis <julius.mazionis@gmail.com>
2012-05-18 01:16:16 +0300

    priority queue

---
+++ OsModel/Planner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ookii.Collections.Generic;
+using OsModel.Processes;
+
+namespace OsModel
+{
+    public static class Planner
+    {
+        public static PriorityQueue<Process> processQueue;
+
+        static Planner()
+        {
+            processQueue = new PriorityQueue<Process>(new ProcessComparer());
+            processQueue.Enqueue(new Process(5));
+            processQueue.Enqueue(new Process(2));
+            processQueue.Enqueue(new Process(3));
+        }
+
+        private class ProcessComparer : IComparer<Process>
+        {
+            public int Compare(Process x, Process y)
+            {
+                return x.Priority - y.Priority;
+            }
+        }
+    }
+}
---
+++ OsModel/Processes/Process.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ookii;
+
+namespace OsModel.Processes
+{
+    public enum State { Blocked, Ready, Active }
+
+    public class Process
+    {
+        public int Priority { get; set; }
+
+        public Process(int priority)
+        {
+            this.Priority = priority;
+        }
+    }
+}
