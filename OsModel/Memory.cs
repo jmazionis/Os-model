99ebd35988e9a396a6d09403d9fd74d7ec3858ba
Tomas Vardanian <bfdenjoy@gmail.com>
2012-05-18 11:36:40 +0300

    Added VM

---
+++ OsModel/Memory.cs
@@ -0,0 +1,75 @@
+﻿namespace OsModel.VirtualMachineEmulator
+{
+
+    public class Memory
+    {
+        private Word[,] words;
+        public const int BLOCK_WORD_COUNT = 16;
+        public const int REAL_MEMORY_BLOCK_COUNT = 16;
+        public const int VIRTUAL_MEMORY_BLOCK_COUNT = 5;
+
+        public Memory(int blockCount, int wordCount)
+        {
+            this.words = new Word[blockCount, wordCount];
+            for (int i = 0; i < this.words.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.words.GetLength(1); j++)
+                {
+                    this.words[i, j] = new Word();
+                }
+            }
+        }
+
+        public int BlockCount 
+        { 
+            get { return this.words.GetLength(0); }
+        }
+
+        public int WordCount
+        {
+            get { return this.words.GetLength(1); }
+        }
+
+        public Word this[int block, int word]
+        {
+            set { words[block, word] = value; }
+            get { return words[block, word]; }
+        }
+
+        public void Write(string operand, string buffer)
+        {
+            int block = Word.HexToInt(operand[0].ToString());
+            Word[] words = StringToWords(buffer);
+            for (int i = 0; i < words.Length; i++)
+                this.words[block, i] = words[i];
+
+        }
+
+        private Word[] StringToWords(string s)
+        {
+            if (s == null)
+            {
+                return null;
+            }
+            int wordCount = s.Length / 4;
+            Word[] words = new Word[wordCount];
+            for (int i = 0; i < wordCount; i++)
+            {
+                words[i] = new Word(s.Substring(i * 4, 4));
+            }
+            string remaining = s.Substring(s.Length - s.Length % 4);
+            if (remaining.Length > 0)
+            {
+                Word[] allWords = new Word[wordCount + 1];
+                words.CopyTo(allWords, 0);
+                allWords[allWords.Length - 1] = new Word(remaining);
+                return allWords;
+            }
+            else
+            {
+                return words;
+            }
+        }
+        
+    }
+}
---
+++ OsModel/VirtualMachine.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace OsModel.VirtualMachineEmulator
+{
+    public class VirtualMachine
+    {
+        public event VMEventHandler VMTaskFinished;
+        private Cpu cpu;
+        private IO io;
+        private Memory memory;
+        private TaskLoader task;
+        public static int BlockOffset = 0;
+
+        public VirtualMachine(string fileName)
+        {
+            memory = new Memory(Memory.VIRTUAL_MEMORY_BLOCK_COUNT, Memory.BLOCK_WORD_COUNT);
+            this.task = new TaskLoader(fileName, memory);
+            task.Load();
+            cpu = new Cpu(memory, this);
+            io = new IO();
+            BlockOffset += Memory.VIRTUAL_MEMORY_BLOCK_COUNT;
+            this.MapBlocks();
+            VMTaskFinished += ReduceOffset;
+        }
+
+        public void MapBlocks()
+        {
+            int rmblock;
+            for (int i = 0; i < BlockOffset; i++)
+            {
+                rmblock = Word.HexToInt(RealMachine.Memory[RealMachine.PTR, i].Value);
+                for (int j = 0; j < Memory.BLOCK_WORD_COUNT; j++)
+                {
+                    RealMachine.Memory[rmblock, j] = this.memory[i, j];
+                }            
+            }
+        }
+
+        private void ReduceOffset()
+        {
+            BlockOffset -= Memory.VIRTUAL_MEMORY_BLOCK_COUNT;
+        }
+
+        public void CheckIfInputNext()
+        {
+            if (cpu.NextCommand().Substring(0, 2) == "GD")
+                io.ReadBuffer();
+        }
+
+        public void ExecuteNext()
+        {
+            if (!cpu.ExecuteNext() && VMTaskFinished != null)
+                VMTaskFinished();
+            CheckIfInputNext();
+        }
+
+        public void ExecuteAll()
+        {
+            while (true)
+            {
+                if (!cpu.ExecuteNext() && VMTaskFinished != null)
+                {
+                    VMTaskFinished();
+                    return;
+                }
+                CheckIfInputNext();
+            }
+        }
+
+        public Memory Memory
+        {
+            get { return memory; }
+        }
+
+        public IO Io
+        {
+            get { return io; }
+        }
+
+        public Cpu Cpu
+        {
+            get { return cpu; }
+        }
+    }
+}
