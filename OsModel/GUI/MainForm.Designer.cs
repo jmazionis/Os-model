9c0e76d1bacc119622ac3b40a0d1fac089f9ee10
Tomas Vardanian <bfdenjoy@gmail.com>
2012-05-26 20:24:22 +0300

    +gui

--- OsModel/GUI/MainForm.Designer.cs
+++ OsModel/GUI/MainForm.Designer.cs
@@ -29,9 +29,11 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.controlsGroupBox = new System.Windows.Forms.GroupBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.controlsGroupBox = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.controlsGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -45,17 +47,6 @@
             this.tabControl1.Size = new System.Drawing.Size(819, 145);
             this.tabControl1.TabIndex = 0;
             // 
-            // controlsGroupBox
-            // 
-            this.controlsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.controlsGroupBox.Location = new System.Drawing.Point(837, 34);
-            this.controlsGroupBox.Name = "controlsGroupBox";
-            this.controlsGroupBox.Size = new System.Drawing.Size(204, 484);
-            this.controlsGroupBox.TabIndex = 1;
-            this.controlsGroupBox.TabStop = false;
-            this.controlsGroupBox.Text = "Controls";
-            // 
             // tabPage3
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
@@ -64,6 +55,27 @@
             this.tabPage3.TabIndex = 0;
             this.tabPage3.Text = "tabPage3";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // controlsGroupBox
+            // 
+            this.controlsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.controlsGroupBox.Controls.Add(this.button1);
+            this.controlsGroupBox.Location = new System.Drawing.Point(837, 34);
+            this.controlsGroupBox.Name = "controlsGroupBox";
+            this.controlsGroupBox.Size = new System.Drawing.Size(204, 484);
+            this.controlsGroupBox.TabIndex = 1;
+            this.controlsGroupBox.TabStop = false;
+            this.controlsGroupBox.Text = "Controls";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start OS";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -75,6 +87,7 @@
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.tabControl1.ResumeLayout(false);
+            this.controlsGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -84,6 +97,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.GroupBox controlsGroupBox;
         private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
 
     }
 }
