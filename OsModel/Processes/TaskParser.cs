79c09875a418fdc6ca21959b838c728ff8759a7b
Tomas Vardanian <bfdenjoy@gmail.com>
2012-05-27 20:33:38 +0300

    parse

--- OsModel/Processes/TaskParser.cs
+++ OsModel/Processes/TaskParser.cs
@@ -17,7 +17,10 @@ namespace OsModel.Processes
 
         private bool Parse() //TODO: implement properly
         {                    //how to check: if taskSource[0] does not belong to virtual machine command set, return false
-                                                        
+            if (!blockTags.Contains(taskSource[0].Substring(0, 2)))
+                return false;
+            if (!taskSource.Memory.Contains("$END"))
+                return false;
             return true; //if no errors were found while parsing taskSource array, consisting of source code of a task, method must return true
         }
 
@@ -45,5 +48,16 @@ namespace OsModel.Processes
                     }
             }
         }
+
+        private static List<string> blockTags = new List<string>()
+        {
+            "$000", "$010", "$020", "$030", "$040" 
+        };
+        /*private static List<string> commands = new List<string>()
+        {
+            "AD", "SU", "CM", "GD", "PD", "JM", 
+            "JE", "JA", "JN", "JB", "JG", "JL", "SA",
+            "SC", "LA", "LC", "$E"
+        };*/
     }
 }
